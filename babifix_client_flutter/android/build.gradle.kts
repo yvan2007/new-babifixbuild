3d689d119002158e628e4d3fbe670f146604199a
Yvan Kouakou <kouayavana20@gmail.com>
2026-05-11 15:13:03 +0000

    fix: ZEGO gradle network config for artifact-node.zego.cloud
    - Ajout des depots maven.zego.im et jitpack.io dans build.gradle.kts
    - Ajout parametres IPv4 dans gradle.properties pour resoudre probleme DNS Java
    - Mise a jour zego_uikit_prebuilt_call vers 4.17.13

--- babifix_client_flutter/android/build.gradle.kts
+++ babifix_client_flutter/android/build.gradle.kts
@@ -2,6 +2,8 @@ allprojects {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://maven.zego.im") }
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
