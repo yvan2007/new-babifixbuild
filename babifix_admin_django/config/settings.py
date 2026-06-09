77f18c67400f607a00a2c631fbcef0aacba49008
yvan2007 <kouayavana20@gmail.com>
2026-06-09 15:53:03 +0000

    fix: add STATIC_ROOT manquant (casse collectstatic sur Render)

--- babifix_admin_django/config/settings.py
+++ babifix_admin_django/config/settings.py
@@ -215,7 +215,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
